9143b24d45c7881d9fec10c7f991904380c500e2
Michel Eter <micoror1@gmail.com>
2019-04-17 00:21:22 -0700

    Half of the SVG images now change accordingly; some layout inconsistencies have been fixed

--- hwk3/source/makeRequest.js
+++ hwk3/source/makeRequest.js
@@ -25,11 +25,30 @@ function makeCorsRequest() {
   xhr.onload = function () {
     let responseStr = xhr.responseText;  // get the JSON string 
     let object = JSON.parse(responseStr);  // turn it into an object
-    current_temp.innerHTML = Math.round(object.list[0].main.temp) + "&#176;";
-    
+
+    let icon = document.getElementById("current_img");
+    let objectIcon = object.list[0].weather[0].icon;
+    if (objectIcon == '01d' || objectIcon == '02d') {
+      icon.src = "C:/Users/micor/OneDrive/Documents/GitHub/ecs162/hwk3/assets/clearsky.svg";
+    } else if (objectIcon == '01n' || objectIcon == '02n') {
+      icon.src = "C:/Users/micor/OneDrive/Documents/GitHub/ecs162/hwk3/assets/clear-night.svg";
+    } else if (objectIcon == '03n' || objectIcon == '03d') {
+      icon.src = "C:/Users/micor/OneDrive/Documents/GitHub/ecs162/hwk3/assets/scatteredclouds.svg";
+    } else if (objectIcon == '04d' || objectIcon == '04n') {
+      icon.src = "C:/Users/micor/OneDrive/Documents/GitHub/ecs162/hwk3/assets/brokencloud.svg";
+    } else if (objectIcon == '10d') {
+      icon.src = "C:/Users/micor/OneDrive/Documents/GitHub/ecs162/hwk3/assets/rain-day.svg";
+    } else {
+      icon.src = "C:/Users/micor/OneDrive/Documents/GitHub/ecs162/hwk3/assets/snow.svg";
+    }
+
+    current_temp.innerHTML = Math.round(object.list[0].main.temp) + "&deg;";
+
     let tempTime = new Date();
     let time = tempTime.toLocaleString('en-US', { hour: 'numeric', hour12: true });
     current_time.innerHTML = time;
+
+    console.log(JSON.stringify(object, undefined, 2));
   };
 
   xhr.onerror = function () {
